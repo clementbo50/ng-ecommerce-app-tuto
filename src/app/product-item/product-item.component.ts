005ea299c41d6ff892a2d2745a80626253941ab0
clementbo50 <clementbo50@gmail.com>
2024-06-24 18:03:22 +0200

    Module 4 : les templates et la gestion d’un composant
    -Mis en place du tuto pour l'application e-commerce

---
+++ src/app/product-item/product-item.component.ts
@@ -0,0 +1,36 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.css'
+})
+export class ProductItemComponent implements OnInit{
+   @Input() imageUrl!: string;
+   @Input() title!: string;
+   @Input() description!: string;
+   @Input() price!: number;
+
+  /**
+   * Événement émis lorsqu'un produit est ajouté au panier.
+   */
+  @Output() addToCartEvent = new EventEmitter<void>();
+
+  /**
+   * Émet l'événement addToCartEvent lorsqu'on l'appelle.
+   * Cette fonction est appelée lorsque le bouton "Ajouter au panier" est cliqué.
+   */
+
+  ngOnInit(): void {
+    console.log(`ProductItemComponent pour ${this.title} a été initialisé`);
+  }
+  addToCartEmitter() {
+    // Émet l'événement addToCartEvent pour notifier le composant parent que le produit a été ajouté au panier.
+    this.addToCartEvent.emit();
+  }
+}
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ProductItemComponent } from '../product-item/product-item.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProductItemComponent
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  products = [
+    { imageUrl: 'https://picsum.photos/200/300', title: 'Product 1', description: 'Description of product 1', price: 10 },
+    { imageUrl: 'https://picsum.photos/200/300?grayscale', title: 'Product 2', description: 'Description of product 2', price: 20 },
+    { imageUrl: 'https://picsum.photos/200/300?blur=2', title: 'Product 3', description: 'Description of product 3', price: 30 },
+  ];
+
+  addToCart(product: any) {
+    console.log('Product added to cart:', product);
+  }
+
+}
